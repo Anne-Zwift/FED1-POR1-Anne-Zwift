9d724c9b02799f73e859bdf1e3aacd6401374171
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-06-16 12:57:32 +0200

    handling the menu bar and closing being automatic

--- javaScript/index.js
+++ javaScript/index.js
@@ -2,10 +2,19 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let gradientBtn = document.querySelector('.gradient-btn');
 let contactSection = document.querySelector('#contact');
+const navLinks = document.querySelectorAll('.navbar a');
 
 menuIcon.addEventListener("click", () => {
   menuIcon.classList.toggle('bx-x');
   navbar.classList.toggle('active');
+});
+
+// Close menu when a nav link is clicked
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbar.classList.remove('active');
+    menuIcon.classList.remove('bx-x');
+  });
 });
 
 gradientBtn.addEventListener("click", (event) => {
